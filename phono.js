4e1978d864b94b0094c9e09bd7c4d047fbcd543c
Stuart Sackler <stuart.sackler@gmail.com>
2019-05-22 23:52:47 -0400

    initial file upload

---
+++ phono.js
@@ -0,0 +1,67 @@
+/* 
+    #1: Create 3 variables. One MUST be a constant.
+    - One for each datatype in JavaScript (number, string, boolean)
+    - Use your name & student number for the string / number respectively 
+    - Initilize the boolean to either true or false
+*/
+
+
+
+
+/*
+    #2: Create an Array named fruit with at least 5 different fruit
+*/
+
+
+
+
+/*
+    #3: Create an Object named phone using your phone as reference
+    
+    - Required Properties 
+    -- make
+    -- model 
+    -- color
+    -- isOn (boolean)
+    -- contacts (Array - phone numbers, minimum 2, fake ones please)
+    
+    - Required Methods (functions)
+    -- powerToggle()
+    --- When called, switches the value of isOn
+    
+    -- sendMessage(number, msg)
+    --- When called displays the number & message using console.log();
+    --- Also sends a message to console saying "Message Delivered" using console.log(); 
+*/
+
+
+
+
+/*
+    #4: Display your 3 variables on the page using console.log();
+*/
+
+
+/*
+    #5: Use a Loop to display each fruit in your Array using console.log();
+    (use the built-in Length function to iterate your array ex. fruit.length) 
+*/
+    
+
+
+/*
+    #6: Display your phone properties (ignore contacts) using console.log();
+*/
+
+
+
+/*
+    #7: Toggle phone power using the powerToggle() method 
+    - Display the new value
+*/
+
+
+
+/*
+    #8: Use the phone sendMessage() method to say "Hello" to a number in your contacts array
+*/
